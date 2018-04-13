d09f83a78b285f8d404d06f2617397ff8ad1450c
Cezary Tworzewski <cezary.tworzewski@gmail.com>
2018-04-13 14:28:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,46 @@ $(document).ready(function() {
     $('#test').click(function() {
 		$('#test').hide(3000).show(3000).text('Cezary Tworzewski');
 	});
-	
+});
+
+$(document).ready(function() {
+    $('#test').click(function() {
+		$('#test').hide(3000).show(3000).text('Cezary Tworzewski');
+	});
+});
+
+$(document).ready(function() {
+    $('#test').click(function() {
+		$('#test').hide(3000).show(3000).text('Cezary Tworzewski');
+	});
+});
+
+$(document).ready(function() {
+    $('#test').click(function() {
+		$('#test').hide(3000).show(3000).text('Cezary Tworzewski');
+	});
+});
+
+$(document).ready(function() {
+    $('#test').click(function() {
+		$('#test').hide(3000).show(3000).text('Cezary Tworzewski');
+	});
+});
+
+$(document).ready(function() {
+    $('#test').click(function() {
+		$('#test').hide(3000).show(3000).text('Cezary Tworzewski');
+	});
+});
+
+$(document).ready(function() {
+    $('#test').click(function() {
+		$('#test').hide(3000).show(3000).text('Cezary Tworzewski');
+	});
+});
+
+$(document).ready(function() {
+    $('#test').click(function() {
+		$('#test').hide(3000).show(3000).text('Cezary Tworzewski');
+	});
 });
