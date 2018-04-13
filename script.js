a446142285126bfaff32dc4158e6f5d073527bb6
Cezary Tworzewski <cezary.tworzewski@gmail.com>
2018-04-13 21:42:34 +0200

    Add to button effect opacity in jQuery

--- script.js
+++ script.js
@@ -2,5 +2,11 @@ $(document).ready(function() {
     $('#test').click(function() {
 		$('#test').hide(3000).show(3000).text('Cezary Tworzewski').css('color', 'red', 'text-align', 'center', 'font-size', '25px');
 	});
+	
+	$('button').hover(function() {
+		$(this).siblings('button').css('opacity', '0.3');
+	},function() {
+		$('button').css('opacity', '1');	
+	});
 });
 
